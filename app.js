c8333c6b75c9b24b82436b52360c74fd0f346718
IGardocki <ian.d.gardocki@gmail.com>
2022-10-04 22:23:52 -0500

    updated cors

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express'); // import express module (simplifies routing
 const app = express(); // create an instance of the express module (app is the conventional variable name used)
 const fetch = require('node-fetch'); // import node-fetch (enables the fetch API to be used server-side)
 const PORT = process.env.PORT || 5000; // use either the host env var port (PORT) provided by Heroku or the local port (5000) on your machine
+const cors = require('cors');
+
+app.use(cors())
 
 app.get('/', (req, res) => { // send a get request to root directory ('/' is this file (app.js))
   fetch('https://www.boredapi.com/api/activity') // fetch activity from bored API - https://www.boredapi.com/about
